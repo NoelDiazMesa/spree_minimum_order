4448e06441a407edba8a54b396b4a66631330e17
Noel <noel@2bedigital.com>
2015-10-19 07:12:02 +0100

    added install order_decorator

--- lib/generators/spree_minimum_order/install/install_generator.rb
+++ lib/generators/spree_minimum_order/install/install_generator.rb
@@ -1,7 +1,10 @@
 module SpreeMinimumOrder
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
+      source_root File.expand_path('../../templates', __FILE__)
+      def copy_order_file
+        copy_file "app/models/spree/order_decorator", "app/models/spree/order_decorator"
+      end
     end
   end
 end
