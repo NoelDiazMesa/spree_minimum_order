97b0f3b1763e1119004bb5c8c51339851c1b31db
Noel <noel@2bedigital.com>
2015-10-19 06:38:27 +0100

    modified lib/spree_minimum_order.rb

--- app/models/spree/order_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Order.class_eval do
-  MINIMUM_ORDER_VALUE = Spree::Config[:minimum_order_value].to_f
-  def checkout_allowed?
-    return :not_empty     unless line_items.count > 0
-    return :minimum_order_value unless total >= MINIMUM_ORDER_VALUE
-    true
-  end
-end
--- lib/spree_minimum_order.rb
+++ lib/spree_minimum_order.rb
@@ -1,4 +1,1 @@
-require 'spree_backend'
-require 'spree_frontend'
-require 'spree_core'
 require 'spree_minimum_order/engine'
